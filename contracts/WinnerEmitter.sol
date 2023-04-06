1befee5919b63af9e64332ff576d22a6868e132c
eduairet <hola@eduairet.com>
2023-04-05 19:22:39 -0600

    DeployScript - Compiled

--- contracts/WinnerEmitter.sol
+++ contracts/WinnerEmitter.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
 contract WinnerEmitter {
     uint public x;
+
+    function changeX(uint _x) external {
+        x = _x;
+    }
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,20 @@
 import { ethers } from 'hardhat';
+const { getContractFactory } = ethers;
 
-async function main() {}
+async function main(): Promise<void> {
+    const WinnerEmitter = await getContractFactory('WinnerEmitter'),
+        winnerEmitter = await WinnerEmitter.deploy();
+
+    await winnerEmitter.deployed();
+
+    console.log(`WinnerEmitter address: ${winnerEmitter.address}`);
+    if (winnerEmitter.deployTransaction.chainId === 5) {
+        console.log(`Block explorer:`);
+        console.log(
+            `https://goerli.etherscan.io/address/${winnerEmitter.address}`
+        );
+    }
+}
 
 main()
     .then(() => (process.exitCode = 1))
